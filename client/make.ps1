020a7e7dd378594188385bc9eb8b66ca8cc53f9e
Allan Callaghan <calaldees@gmail.com>
2015-01-09 22:15:46 +0000

    Additions for dependencys for windows powershell make

--- client/make.ps1
+++ client/make.ps1
@@ -1,11 +1,14 @@
 param ( [Switch] $install = $false, [Switch] $run = $false )
 
 # set-executionpolicy -unrestricted
+# http://superuser.com/questions/25538/how-to-download-files-from-command-line-in-windows-like-wget-is-doing
 
 if ($install -eq $true ) {
-    Invoke-WebRequest "http://" -OutFile "test.txt"
+    Invoke-WebRequest "https://raw.githubusercontent.com/calaldees/PentatonicHero/master/network_display_event.py" -OutFile "network_display_event.py"
+    Invoke-WebRequest "https://raw.githubusercontent.com/calaldees/PentatonicHero/master/pygame_midi_wrapper.py"   -OutFile "pygame_midi_wrapper.py"
+    Invoke-WebRequest "https://raw.githubusercontent.com/calaldees/PentatonicHero/master/music.py"                 -OutFile "music.py"
 }
 
 if ($run -eq $true) {
-    python3 displaytrigger.py
+    python3 displaytrigger.py --input midi
 }
