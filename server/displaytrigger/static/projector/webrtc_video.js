3f3888c06905853a245c3fc47503fd52d8eecf41
Allan Callaghan <calaldees@gmail.com>
2015-06-10 19:02:43 +0100

    Got WebRTC to open a video feed, but only gets the first frame rather than opening a stream

--- server/displaytrigger/static/projector/webrtc_video.js
+++ server/displaytrigger/static/projector/webrtc_video.js
@@ -1,3 +1,5 @@
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
 var webrtc_video = {};
 
 
@@ -5,37 +7,52 @@ var webrtc_video = {};
 
 (function(external, options){
 	options = _.extend({
-		selector_holder: '#overlay',
+		selector_holder: '#offscreen_video_feeds',
 	}, options);
 	
-    MediaStreamTrack.getSources(function (media_sources) {
-        _.each(_.filter(media_sources, function(media_source){return media_source.kind == 'video'}), function(media_source, index, list){
-            var constraints = {
-                video: {
-                    optional: [{
-                        sourceId: media_source.id
-                    }],
-                }
-            };
-        
-            console.log(constraints);
-        
-            // invoke getUserMedia to capture this device
-            /*
-            navigator.webkitGetUserMedia(
-                constraints,
-                function (stream) {
-                    console.log(stream.id, stream);
-                },
-                function () {
-                    console.log('its fucked');
-                }
-            );
-            */
-        });
-    });
+	var streams = [];
+	
+	function errorCallback(error) {
+		console.log('error: ', error);
+	}
+	
+	function assignStreamToVideoElement(stream, video_element) {
+		if (window.URL) {
+			video_element.src = window.URL.createObjectURL(stream);
+		} else {
+			video_element.src = stream;
+		}
+	}
+	
+	function getAllVideoDevices(openStreamSuccessCallback, openStreamErrorCallback) {
+		MediaStreamTrack.getSources(function (media_sources) {
+			_.each(_.filter(media_sources, function(media_source){return media_source.kind == 'video'}), function(media_source, index, list){
+				var constraints = {
+					audio: false,
+					video: {
+						optional: [{
+							sourceId: media_source.id
+						}],
+					},
+				};
+				navigator.getUserMedia(constraints, openStreamSuccessCallback, openStreamErrorCallback);
+			});
+			
+		});
+	}
+	
+	function openStream(stream) {
+		streams.push(stream);
+		var $video = $('<video/>');
+		assignStreamToVideoElement(stream, $video[0]);
+		$(options.selector_holder).append($video);
+	}
 
-	//external.display = display;
+	function initVideoDevices() {
+		getAllVideoDevices(openStream, errorCallback);
+	}
+
+	external.initVideoDevices = initVideoDevices;
 }(
 	webrtc_video,{
 	}
