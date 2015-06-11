f750eef2037f07ecad7ace50fd908a9720d929e5
Allan Callaghan <calaldees@gmail.com>
2015-06-11 07:52:49 +0100

    Realtime video draws to on screen canvas

--- server/displaytrigger/static/projector/webrtc_video.js
+++ server/displaytrigger/static/projector/webrtc_video.js
@@ -2,15 +2,18 @@ navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia 
 
 var webrtc_video = {};
 
+// http://stackoverflow.com/questions/12160174/always-accept-webrtc-webcam-request
+//  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --use-fake-ui-for-media-stream
 
 // Display ---------------------------------------------------------------------
 
 (function(external, options){
 	options = _.extend({
 		selector_holder: '#offscreen_video_feeds',
+		fps: 5,
 	}, options);
 	
-	var streams = [];
+	var videos = [];
 	
 	function errorCallback(error) {
 		console.log('error: ', error);
@@ -30,6 +33,10 @@ var webrtc_video = {};
 				var constraints = {
 					audio: false,
 					video: {
+						mandatory: {
+							minWidth: 640, minHeight: 480,
+							maxWidth: 1024, maxHeight: 768,
+						},
 						optional: [{
 							sourceId: media_source.id
 						}],
@@ -41,15 +48,37 @@ var webrtc_video = {};
 		});
 	}
 	
-	function openStream(stream) {
-		streams.push(stream);
+	function openNewStream(stream) {
 		var $video = $('<video/>');
-		assignStreamToVideoElement(stream, $video[0]);
-		$(options.selector_holder).append($video);
+		$video.attr('autoplay', true);  // http://stackoverflow.com/questions/20822833/only-one-frame-displayed-webrtc-stream
+		var video = $video[0];
+		assignStreamToVideoElement(stream, video);
+		var $canvas = $('<canvas/>');
+		var canvas = $canvas[0];
+		canvas.width = 320;
+		canvas.height = 200;
+		var context = canvas.getContext('2d');
+		
+		videos.push({
+			stream: stream,
+			canvas: canvas,
+			conext: context,
+		});
+		
+		var $container = $(options.selector_holder);
+		$container.append($video);
+		$container.append($canvas);
+		
+		function update_video_canvas() {
+			if (video.paused || video.ended) {return};
+			context.drawImage(video, 0, 0, canvas.width, canvas.height);
+			setTimeout(update_video_canvas, Math.floor(1000/options.fps));
+		};
+		$video.on('play', update_video_canvas);
 	}
 
 	function initVideoDevices() {
-		getAllVideoDevices(openStream, errorCallback);
+		getAllVideoDevices(openNewStream, errorCallback);
 	}
 
 	external.initVideoDevices = initVideoDevices;
