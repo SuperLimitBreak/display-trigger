7c0980bc56ed1902dc2a803850f280f0d36b0063
Allan Callaghan <calaldees@gmail.com>
2015-07-22 19:06:20 +0100

    Attempted to make background_scroller have external options and triggerable

--- server/displaytrigger/static/projector/background_scroller.js
+++ server/displaytrigger/static/projector/background_scroller.js
@@ -2,9 +2,9 @@ var background_scroller = {};
 
 
 (function(external, options) {
+
 	// Constants ---------------------------------------------------------------
 
-	var DEFAULT_THING = 5;
 	var BACKGROUND_CSS = {
 		position: 'absolute',
 		top: 0,
@@ -20,136 +20,44 @@ var background_scroller = {};
 		overflow: 'hidden',
 	};
 
+
 	// Options -----------------------------------------------------------------
 
-	options = _.extendOwn({
-		thing: DEFAULT_THING,
+	options = _.extend({
+		selector_holder: '#screen',
+		backgrounds: {},
+		scrolls: {},
 	}, options);
 
 
-	// Variables ---------------------------------------------------------------
-
-	var css = {};
-
-	// TODO: move backgrounds and scrolls to 'options'
-	var backgrounds = {
-		castelvania_1: {
-			background_url: '/ext/castlevaniafullgamemapempty.PNG',
-			source_screen_height: 168,
-			source_width: 9928,
-			source_height: 1908,
-		},
-		castelvania_sotn: {
-			background_url: '/ext/sotn-castle.png',
-			source_screen_height: 206,
-			source_width: 15648,
-			source_height: 12000,
-		},
-		super_metroid: {
-			background_url: '/ext/SuperMetroidMapZebes.png',
-			source_screen_height: 240,
-			source_width: 16896,
-			source_height: 14336,			
-		},
-		super_metroid_cut: {
-			background_url: '/ext/SuperMetroidMapZebes_cut.png',
-			source_screen_height: 240,
-			source_width: 3074,
-			source_height: 240,
-		}
-	}
-	
-	var scrolls = [
-		/*
-		_.extend({}, backgrounds.super_metroid_cut, {
-			name: "Super Metroid: Morph ball",
-			startX: 0,
-			startY: 0,
-			endX: -2700,
-			duration: '10s',
-		}),
-		*/
-		_.extend({}, backgrounds.castelvania_1, {
-			name: "Castelvania: Outside",
-			startX: 0,
-			startY: -1563,
-			endX: -752,
-			duration: '5s',
-		}),
-		_.extend({}, backgrounds.castelvania_1, {
-			name: "Castelvania: First hall",
-			startX: -767,
-			startY: -1550,
-			endX: -2304,
-			duration: '5s',
-		}),
-		_.extend({}, backgrounds.castelvania_1, {
-			name: "Castelvania: Bridge",
-			startX: -2096,
-			startY: -590,
-			endX: -4510,
-			duration: '5s',
-		}),
-		_.extend({}, backgrounds.castelvania_1, {
-			name: "Castelvania: Drop",
-			startX: -4260,
-			startY: -590,
-			endY: -1896,
-			duration: '2s',
-		}),
-		_.extend({}, backgrounds.castelvania_1, {
-			name: "Castelvania: Cave",
-			startX: -4256,
-			startY: -1724,
-			endX: -5790,
-			duration: '5s',
-		}),
-		_.extend({}, backgrounds.castelvania_1, {
-			name: "Castelvania: Bridge 2",
-			startX: -7610,
-			startY: -1026,
-			endX: -6106,
-			duration: '5s',
-		}),		
-
-		/*
-		_.extend({}, backgrounds.castelvania_sotn, {
-			startX: -528,
-			startY: -8968,
-			endX: 2000,
-			duration: '30s',
-		}),
-		_.extend({}, backgrounds.super_metroid, {
-			startX: -4100,
-			startY: -7200,
-			endX: -6916,
-			duration: '10s',
-		}),
-		*/
-	]
-	
 	// Functions ---------------------------------------------------------------
 
 	// Cache load fix - http://mikefowler.me/2014/04/22/cached-images-load-event/
 	function on_image_load($image, f) {
 		$image.on('load', f);
 		if ($image[0].complete) {
-		  $image.load();
-		}	
+			$image.load();
+		}
 	}
 	
-	function scroller($parent) {
-		var _scrolls = _.clone(scrolls).reverse();
+	function start_scroll_sequence(data) {
+		var name = data.name || data;
+		var _scrolls = _.map(options.scrolls[name], function(scroll_item_data){
+			var background_item_data = options.backgrounds[scroll_item.background] || {};
+			return _.extend({}, background_item_data, scroll_item_data);
+		}).reverse();
 		function next() {
-			$(this).off('transitionend');
-			setup_background($parent, _scrolls.pop(), null, next);
+			$(this).off('transitionend');  // WTF is this for?
+			scroll_item(_scrolls.pop(), null, next);
 		}
 		next();
 	}
 	
-	function setup_background($element, params, on_start, on_end) {
-console.log(params);
-
+	function scroll_item(scroll_params, on_start, on_end, _options) {
+		var options = _.extend({}, options, _options);
+		var $element = $(options.selector_holder);
+console.log(scroll_params);
+		
 		if (!on_start) {
 			on_start = function() {};
 		}
@@ -157,14 +65,14 @@ console.log(params);
 			on_end = function() {};
 		}
 		
-		if (typeof(params.endX) != 'number') {
-			params.endX = params.startX
+		if (typeof(scroll_params.endX) != 'number') {
+			scroll_params.endX = scroll_params.startX
 		}
-		if (typeof(params.endY) != 'number') {
-			params.endY = params.startY
+		if (typeof(scroll_params.endY) != 'number') {
+			scroll_params.endY = scroll_params.startY
 		}
 
-		var ratio = $element.innerHeight() / params.source_screen_height;
+		var ratio = $element.innerHeight() / scroll_params.source_screen_height;
 		function px(value) {
 			return ''+(value*ratio)+'px';
 		}
@@ -172,7 +80,7 @@ console.log(params);
 			return ''+px(x)+' '+px(y);
 		}
 		var $image = $element.find('img');
-		var image_in_dom = ($image.attr('src') == params.background_url);
+		var image_in_dom = ($image.attr('src') == scroll_params.background_url);
 
 		if (!image_in_dom) {
 			$element.empty();
@@ -184,33 +92,33 @@ console.log(params);
 		}
 
 		function transition_image() {
-			var translateX = params.endX - params.startX;
+			var translateX = scroll_params.endX - scroll_params.startX;
 			var translateY = 0;
-			if (params.endX < params.startX) {
+			if (scroll_params.endX < scroll_params.startX) {
 				translateX += $element.innerWidth() / ratio;
 			}
-			if (params.endX > params.startX) {
+			if (scroll_params.endX > scroll_params.startX) {
 				translateX += -$element.innerWidth() / ratio;
 			}
-			if (params.endY < params.startY) {
-				translateY = (params.endY - params.startY) + $element.innerHeight() / ratio;
+			if (scroll_params.endY < scroll_params.startY) {
+				translateY = (scroll_params.endY - scroll_params.startY) + $element.innerHeight() / ratio;
 			}
 			// todo: inverse y direction
 
 			$image.css({
-				transition: 'all '+params.duration+' linear',
+				transition: 'all '+scroll_params.duration+' linear',
 				transform: 'translateX('+px(translateX)+') translateY('+px(translateY)+')',
 			});
 			$image.on('transitionend', on_end);
 			on_start();
 		}
 		function postion_image() {
-			var startX = params.startX;
-			var startY = params.startY;
-			if (params.endX > params.startX) {
+			var startX = scroll_params.startX;
+			var startY = scroll_params.startY;
+			if (scroll_params.endX > scroll_params.startX) {
 				startX += $element.innerWidth() / ratio;
 			}
-			if (params.endY > params.startY) {
+			if (scroll_params.endY > scroll_params.startY) {
 				startY += $element.innerHeight() / ratio;
 			}
 			$image.css({
@@ -223,11 +131,11 @@ console.log(params);
 
 		if (!image_in_dom) {
 			on_image_load($image, transition_image);
-			$image.attr('src', params.background_url);
+			$image.attr('src', scroll_params.background_url);
 			$image.css({
 				position: 'absolute',
-				width: px(params.source_width),
-				height: px(params.source_height),
+				width: px(scroll_params.source_width),
+				height: px(scroll_params.source_height),
 			});
 			postion_image();
 		}
@@ -241,7 +149,9 @@ console.log(params);
 
 	// External Export ---------------------------------------------------------
 	
-	external.setup_background = setup_background;
-	external.scroller = scroller;
+	_.extend(external, {
+		scroll_item: scroll_item,
+		start_scroll_sequence: start_scroll_sequence,
+	});
 
 }(background_scroller, utils.functools.get('options.background_scroller')));
