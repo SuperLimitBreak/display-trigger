832d35d1f3d42b8f94f85bced243230de1c9630d
Allan Callaghan <calaldees@gmail.com>
2015-07-22 08:09:42 +0100

    Moved run_funcs to utils.js

--- server/displaytrigger/static/projector/_init.js
+++ server/displaytrigger/static/projector/_init.js
@@ -1,17 +1,5 @@
 
-function run_funcs(data) {
-	//console.log('message', data);
-	if (_.isArray(data)) {
-		_.each(data, function(element, index, list){
-			run_funcs(element);
-		});
-	}
-	if (_.has(data, 'func')) {
-		utils.functools.get_func(data.func)(data);
-	}
-}
-
 var socket = WebSocketReconnect({
 	onopen: function() {},
-	onmessage: run_funcs,
+	onmessage: utils.functools.run_funcs,
 });
