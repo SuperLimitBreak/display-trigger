f06ad882b203d8eaabd8b8f187d460d5ed25a638
Allan Callaghan <calaldees@gmail.com>
2015-12-15 19:08:34 +0000

    Wrapped socketserver startup in catch

--- server/displaytrigger/__init__.py
+++ server/displaytrigger/__init__.py
@@ -6,6 +6,8 @@ from externals.lib.multisocket.subscription_server import SubscriptionEchoServer
 import logging
 log = logging.getLogger(__name__)
 
+EXCEPTION_NUMBER_ADDRESS_ALREADY_IN_USE = 48
+
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
@@ -16,12 +18,18 @@ def main(global_config, **settings):
         config.registry.settings[key] = convert_str_with_type(config.registry.settings[key])
 
     # TCP/Websocket
-    socket_manager = SubscriptionEchoServerManager(
-        websocket_port=config.registry.settings['multisocket.websocket.port'],
-        tcp_port=config.registry.settings['multisocket.tcp.port'],
-    )
-    config.registry['socket_manager'] = socket_manager
-    socket_manager.start()
+    try:
+        socket_manager = SubscriptionEchoServerManager(
+            websocket_port=config.registry.settings['multisocket.websocket.port'],
+            tcp_port=config.registry.settings['multisocket.tcp.port'],
+        )
+        config.registry['socket_manager'] = socket_manager
+        socket_manager.start()
+    except OSError as ex:
+        if ex.errno == EXCEPTION_NUMBER_ADDRESS_ALREADY_IN_USE:
+            log.warn('Another tcp/websocket server already exists')
+        else:
+            raise ex
 
     config.add_static_view(name='static', path='static')  # , cache_max_age=3600
     config.add_static_view(name='ext', path=settings['static.path.ext'], cache_max_age=3600)
