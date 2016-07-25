989505268ed0640a808f36603fbe88aff618e928
Allan Callaghan <calaldees@gmail.com>
2016-07-25 07:52:25 +0100

    Correct #mask to autosize and use actual mask 'url' form trigger Corrected width of overlay (still need font to resize with parent)

--- display/build/mask.js
+++ display/build/mask.js
@@ -3,7 +3,7 @@ var mask = {};
 (function(external, options){
 	options = _.extend({
 		element_selector: '#mask',
-		element_selector_top_level: 'body',
+		//element_selector_top_level: 'body',
 	}, options);
 
 	function mask(data) {
@@ -12,11 +12,18 @@ var mask = {};
 		//$mask.attr("src", data.src);
 		//$overlay.append($mask);
 		//console.log("do", options.element_selector_top_level, data.css_class);
-		$(options.element_selector_top_level).addClass(data.css_class);
+		
+		//$(options.element_selector_top_level).addClass(data.css_class);
+		var $mask_container = $(options.element_selector);
+		var $mask_image = $("<img>");
+		$mask_image.attr("src", data.src);
+		$mask_container.append($mask_image);
 	}
 
     function empty() {
-        $(options.element_selector_top_level).removeClass("reignite_mask");
+        //$(options.element_selector_top_level).removeClass("reignite_mask");
+		var $mask_container = $(options.element_selector);
+		$mask_container.clear();
     }
     
     $.subscribe('trigger.stop', empty);
