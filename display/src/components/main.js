a1333d149acad3bce29e75bf0364b78549b6183f
Allan Callaghan <calaldees@gmail.com>
2016-08-18 08:09:46 +0100

    First basic use of es6 and scss

--- display/src/components/main.js
+++ display/src/components/main.js
@@ -1,1 +1,16 @@
 require('normalize.css/normalize.css');
+require('styles/main.scss');
+
+
+class App {
+    constructor(element) {
+        this.element = element;
+        this.element.classList.add('item');
+        this.element.innerText = 'test';
+        //console.log('App constructor');
+    }
+}
+
+export default {
+    App: new App(document.getElementById('app'))
+};
--- display/src/index.js
+++ display/src/index.js
@@ -1,2 +1,6 @@
 import 'core-js/fn/object/assign';
 import App from './components/main';
+
+//console.log(App);
+//console.log("poo");
+
