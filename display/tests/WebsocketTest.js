692a0f62c7115b5aa981033eb93a009a4eaddc56
Allan Callaghan <calaldees@gmail.com>
2016-09-05 19:10:37 +0100

    Shuffled test around to have reuseable components.

--- display/tests/WebsocketTest.js
+++ display/tests/WebsocketTest.js
@@ -3,25 +3,27 @@ import {SocketReconnect, JsonSocketReconnect, SubscriptionSocketReconnect} from 
 
 describe('SocketReconnect', function() {
 
+    let socket;
+
     let mockSocket;
     class MockWebSocket {
         constructor() {
+            expect(mockSocket).toBe(undefined);  // We only ever want one socket connected under test conditions
             mockSocket = jasmine.createSpyObj('WebSocket', ['send', 'onopen', 'onclose', 'onmessage']);
             return mockSocket;
         }
     }
 
     beforeEach(function() {
-        //mockWebSocket = jasmine.createSpyObj('WebSocket', ['send', 'onopen', 'onclose', 'onmessage']);
-    });
-    
-    it('should connect on creation',()=>{
-        let socket = new SocketReconnect({
+        socket = new SocketReconnect({
             WebSocket: MockWebSocket,
         });
         expect(mockSocket).not.toBe(undefined);
         spyOn(socket, 'onConnected').and.callThrough();
         spyOn(socket, 'onDisconnected').and.callThrough();
+    });
+    
+    it('Should called onConnected on creation',()=>{
         expect(socket.onConnected).not.toHaveBeenCalled();
         mockSocket.onopen();
         expect(socket.onConnected).toHaveBeenCalled();
