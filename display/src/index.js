08632b2c4833eccd6a8ce70eb9cae47c0a7b5c95
Allan Callaghan <calaldees@gmail.com>
2016-10-05 07:47:00 +0100

    Screen setup and config now from json data from query string

--- display/src/index.js
+++ display/src/index.js
@@ -2,19 +2,48 @@ import 'core-js/fn/object/assign';
 
 import {ScreenMessageRouter} from './screen/ScreenMessageRouter';
 import {SubscriptionSocketReconnect} from './socket/websocket';
+import {getUrlParameter, static_url} from './utils/utils';
+
+const Immutable = require('immutable');
 
 require('normalize.css/normalize.css');
 require('./styles/main.scss');
+
+
+const body = document.getElementsByTagName('body').item(0);
+
+const DEFAULT_SCREEN_CONFIG = new Immutable.Map({
+    'main': new Immutable.Map({
+        'id': 'default_screen',
+        'classList': undefined,
+        'style': undefined,
+        'subscriptions': [],
+    }),
+});
 
 const screenMessageRouter = new ScreenMessageRouter(
     new SubscriptionSocketReconnect()
 );
 
-const body = document.getElementsByTagName('body').item(0);
-const screen_test = document.createElement('div');
-body.appendChild(screen_test);
+function initScreens(config) {
+    for (let [screen_name, screen_data] of config) {
+        const id = screen_data.get('id');
+        let element = document.getElementById(id);
+        if (!element) {
+            element = document.createElement('div');
+            element.id = id;
+            body.appendChild(element);
+        }
+        //element.classList.concat(screen_data.get('classList'));
+        element.style = screen_data.get('style');
+        screenMessageRouter.bindScreen(screen_name, element, screen_data.get('subscritpions'));
+    }
+}
+  
+const config_url = static_url(`/config/${getUrlParameter('config') || 'default'}.json`);
+fetch(config_url).then(response => {
+    initScreens(Immutable.Map(response.data));
+}).catch(error => {
+    initScreens(DEFAULT_SCREEN_CONFIG);
+});
 
-screenMessageRouter.bindScreen('main', screen_test);
-//screenManager.bindScreen('test', null);
-
-  //{subscriptions:['main', 'all']}
--- display/src/utils/utils.js
+++ display/src/utils/utils.js
@@ -3,3 +3,17 @@ export function static_url(path) {
     if (path && path.indexOf('://')>=0) {return path;}
     return `http://${window.location.hostname}:${HOST_STATIC_PORT}${path}`;
 }
+
+
+export function getUrlParameter(sParam) {
+    //http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+    return '';
+}
