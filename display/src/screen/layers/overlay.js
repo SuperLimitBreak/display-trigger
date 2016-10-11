d7974e72612a991d61d20014df6fd003bc5dfcb4
Allan Callaghan <calaldees@gmail.com>
2016-10-11 18:54:07 +0100

    Implemented fade layer

--- display/src/screen/layers/overlay.js
+++ display/src/screen/layers/overlay.js
@@ -1,4 +1,23 @@
 export class overlay {
-    constructor() {}
+    constructor(element) {
+        this.element = element;
+        this.timeline = undefined;
+    }
+    
+    fade() {
+        this.timeline = new TimelineMax({onComplete:()=>this.empty()});
+        this.timeline.fromTo(this.element, 1,
+            {opacity:0, backgroundColor: 'black'},
+            {opacity:1, backgroundColor: 'black'},
+        );
+    }
+    
+    empty() {
+        if (this.timeline) {
+            this.timeline.stop();
+            this.timeline = undefined;
+        }
+        this.element.style = '';
+    }
 }
 overlay.className = 'overlay';
