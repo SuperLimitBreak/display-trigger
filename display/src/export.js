241264b4e97696de68dc65d907e49028701f9d53
Allan Callaghan <calaldees@gmail.com>
2016-12-07 07:48:57 +0000

    Moved + created base webpack config builder Fixed export.js warning

--- display/src/export.js
+++ display/src/export.js
@@ -1,7 +1,12 @@
 import {ScreenMessageRouter} from './screen/ScreenMessageRouter';
 import {SubscriptionSocketReconnect} from './socket/websocket';
+import {getUrlParameter, static_url} from './utils/utils';
+
 
 module.exports = {
     ScreenMessageRouter,
     SubscriptionSocketReconnect,
+    utils: {
+        getUrlParameter,
+    },
 };
