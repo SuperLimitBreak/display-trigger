7075dc13f784cbd070f3fb2778bb5fe4a440fc89
Allan Callaghan <calaldees@gmail.com>
2016-12-27 15:50:33 +0000

    Rejibbled karma.conf to use local webpack.config.js

--- display/karma.conf.js
+++ display/karma.conf.js
@@ -3,7 +3,9 @@
 // Set node environment to testing
 process.env.NODE_ENV = 'test';
 
-const {buildWebpackCfg} = require('./webpack.config.base.js');
 const {buildKarmaCfg} = require('./karma.conf.base.js');
 
-module.exports = buildKarmaCfg(buildWebpackCfg(__dirname), {});
+module.exports = buildKarmaCfg(
+    require('./webpack.config.js'),
+    {}
+);
