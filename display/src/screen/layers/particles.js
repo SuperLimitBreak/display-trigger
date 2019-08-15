17d51d70b0fdd5b9afe336d6297cf19c1acc6b04
Allan Callaghan <calaldees@gmail.com>
2019-08-15 18:34:24 +0100

    More pixi particles wip What do I do with the static particle files!?

--- display/src/screen/layers/particles.js
+++ display/src/screen/layers/particles.js
@@ -14,6 +14,8 @@ require('../../styles/layers/particles.scss');
 
 
 const DEFAULT_PATH_MEDIA = '/';  // TODO: Import this from a central location
+const DEFAULT_PARTICLE_IMAGES = ['pixiParticles/default.png', ];
+const DEFAULT_TIME_FACTOR = 0.001;
 
 
 export class particles {
@@ -24,7 +26,7 @@ export class particles {
             mediaUrl: (new URLSearchParams(window.location.search)).get('path_media') || DEFAULT_PATH_MEDIA,
         }, kwargs);
 
-        this._timestamp = undefined;
+        this._requestAnimationFrameTimestamp = undefined;
         this._updateAnimationFrameId = undefined;
 
         this._canvas = document.createElement('canvas');
@@ -37,38 +39,73 @@ export class particles {
             },
         )
         this._pixi_container_root = new PIXI.Container();
-
         this._pixi_container_emitter = new PIXI.Container();
         this._pixi_container_root.addChild(this._pixi_container_emitter);
-        emitter = new PIXI.particles.Emitter(
-            emitterContainer,
-            art,
-            config
-        );
+
+        this._emitter = undefined;
+
+        this.start({
+            emmitterConfig: {
+                "alpha": {"start": 0.62, "end": 0},
+                "scale": {"start": 0.25, "end": 0.75},
+                "color": {"start": "fff191", "end": "ff622c"},
+                "speed": {"start": 500, "end": 500},
+                "startRotation": {"min": 265, "max": 275},
+                "rotationSpeed": {"min": 50, "max": 50},
+                "lifetime": {"min": 0.1, "max": 0.75},
+                "blendMode": "normal",
+                "frequency": 0.001,
+                "emitterLifetime": 0,
+                "maxParticles": 1000,
+                "pos": {"x": 0, "y": 0},
+                "addAtBack": false,
+                "spawnType": "circle",
+                "spawnCircle": {"x": 0, "y": 0, "r": 10},
+            },
+        });
     }
 
 
     pause(msg) {return this.stop(msg);}  // TODO: remove alias?
     stop(msg) {
         cancelAnimationFrame(this._updateAnimationFrameId);
+        this._updateAnimationFrameId = undefined;
     }
 
     start(msg) {
         this.stop();
-        this._timestamp = Date.now();
+
+        const images = Array.isArray(msg.particleImages) ? msg.particleImages : DEFAULT_PARTICLE_IMAGES;
+        this._emitter = new PIXI.particles.Emitter(
+            this._pixi_container_emitter,
+            images.map(PIXI.Texture.fromImage),
+            msg.emitterConfig,
+        );
+
+        const timeFactor = msg.timeFactor ? msg.timeFactor : DEFAULT_TIME_FACTOR;
+        this._requestAnimationFrameTimestamp = Date.now();
         const update = function() {
             this._updateAnimationFrameId = requestAnimationFrame(update);
             const now = Date.now();
-            if (emitter) {
-                emitter.update((now - this._timestamp) * 0.001);
+            if (this._emitter) {
+                this._emitter.update((now - this._requestAnimationFrameTimestamp) * timeFactor);
             }
-            this._timestamp = now;
+            this._requestAnimationFrameTimestamp = now;
             this._pixi_renderer.render(this._pixi_container_root);
         };
         update();
     }
 
     empty() {
+        this.stop();
+        this._emitter.destroy();
+        this._emitter = undefined;
+        //reset SpriteRenderer's batching to fully release particles for GC
+        const r = this._pixi_renderer;
+        if (r.plugins && r.plugins.sprite && r.plugins.sprite.sprites) {
+            r.plugins.sprite.sprites.length = 0;
+        }
+        this._pixi_renderer.render(this._pixi_container_root);
     }
 
 }
