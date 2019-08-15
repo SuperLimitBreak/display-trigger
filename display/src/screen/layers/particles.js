aec6a163d6cf500300d82343e3727afb0ad1e0f5
Allan Callaghan <calaldees@gmail.com>
2019-08-15 09:17:40 +0100

    pixi particles wip

--- display/src/screen/layers/particles.js
+++ display/src/screen/layers/particles.js
@@ -1,5 +1,10 @@
 // https://greensock.com/forums/topic/15987-can-you-get-time-since-last-update-from-the-timeline/
 // https://github.com/pixijs/pixi-particles
+// https://www.npmjs.com/package/pixi-particles
+// view-source:https://pixijs.io/pixi-particles/examples/flame.html
+// view-source:https://pixijs.io/pixi-particles/examples/js/ParticleExample.js
+// https://jameskiefer.com/posts/getting-started-with-pixi.js-and-webpack/
+
 
 import * as PIXI from 'pixi.js'
 import * as PIXI_particles from 'pixi-particles';
@@ -19,25 +24,48 @@ export class particles {
             mediaUrl: (new URLSearchParams(window.location.search)).get('path_media') || DEFAULT_PATH_MEDIA,
         }, kwargs);
 
+        this._timestamp = undefined;
+        this._updateAnimationFrameId = undefined;
+
         this._canvas = document.createElement('canvas');
         this.element.appendChild(this._canvas);
-        this._renderer = PIXI.autoDetectRenderer(
+        this._pixi_renderer = PIXI.autoDetectRenderer(
             this._canvas.width,
             this._canvas.height,
             {
-                view: this._canvas
+                view: this._canvas,
             },
         )
-        //this._pixi = new PIXI.Container();
+        this._pixi_container_root = new PIXI.Container();
+
+        this._pixi_container_emitter = new PIXI.Container();
+        this._pixi_container_root.addChild(this._pixi_container_emitter);
+        emitter = new PIXI.particles.Emitter(
+            emitterContainer,
+            art,
+            config
+        );
     }
 
 
     pause(msg) {return this.stop(msg);}  // TODO: remove alias?
     stop(msg) {
-
+        cancelAnimationFrame(this._updateAnimationFrameId);
     }
 
     start(msg) {
+        this.stop();
+        this._timestamp = Date.now();
+        const update = function() {
+            this._updateAnimationFrameId = requestAnimationFrame(update);
+            const now = Date.now();
+            if (emitter) {
+                emitter.update((now - this._timestamp) * 0.001);
+            }
+            this._timestamp = now;
+            this._pixi_renderer.render(this._pixi_container_root);
+        };
+        update();
     }
 
     empty() {
