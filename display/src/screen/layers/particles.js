aa7f3146add6c0d5b7f9ce2b11e6038d5da28a2c
Allan Callaghan <calaldees@gmail.com>
2019-08-16 18:16:34 +0100

    I am the god of hell and fire, and I bring you FIRE!!!!

--- display/src/screen/layers/particles.js
+++ display/src/screen/layers/particles.js
@@ -26,6 +26,8 @@ export class particles {
             mediaUrl: (new URLSearchParams(window.location.search)).get('path_media') || DEFAULT_PATH_MEDIA,
         }, kwargs);
 
+        this._update = this._update.bind(this);
+
         this._requestAnimationFrameTimestamp = undefined;
         this._updateAnimationFrameId = undefined;
 
@@ -43,26 +45,6 @@ export class particles {
         this._pixi_container_root.addChild(this._pixi_container_emitter);
 
         this._emitter = undefined;
-
-        this.start({
-            emmitterConfig: {
-                "alpha": {"start": 0.62, "end": 0},
-                "scale": {"start": 0.25, "end": 0.75},
-                "color": {"start": "fff191", "end": "ff622c"},
-                "speed": {"start": 500, "end": 500},
-                "startRotation": {"min": 265, "max": 275},
-                "rotationSpeed": {"min": 50, "max": 50},
-                "lifetime": {"min": 0.1, "max": 0.75},
-                "blendMode": "normal",
-                "frequency": 0.001,
-                "emitterLifetime": 0,
-                "maxParticles": 1000,
-                "pos": {"x": 0, "y": 0},
-                "addAtBack": false,
-                "spawnType": "circle",
-                "spawnCircle": {"x": 0, "y": 0, "r": 10},
-            },
-        });
     }
 
 
@@ -75,25 +57,31 @@ export class particles {
     start(msg) {
         this.stop();
 
+        if (!msg.emitterConfig) {
+            console.warn('No emitterConfig provided');
+            return;
+        }
+
         const images = Array.isArray(msg.particleImages) ? msg.particleImages : DEFAULT_PARTICLE_IMAGES;
         this._emitter = new PIXI_particles.Emitter(
             this._pixi_container_emitter,
-            images.map(PIXI.Texture.from),
+            images.map(PIXI.Texture.fromImage),
             msg.emitterConfig,
         );
+        this._emitter.updateOwnerPos(this._canvas.width / 2, this._canvas.height / 2);
 
-        const timeFactor = msg.timeFactor ? msg.timeFactor : DEFAULT_TIME_FACTOR;
         this._requestAnimationFrameTimestamp = Date.now();
-        const update = function() {
-            this._updateAnimationFrameId = requestAnimationFrame(update);
-            const now = Date.now();
-            if (this._emitter) {
-                this._emitter.update((now - this._requestAnimationFrameTimestamp) * timeFactor);
-            }
-            this._requestAnimationFrameTimestamp = now;
-            this._pixi_renderer.render(this._pixi_container_root);
-        };
-        update();
+        this._update();
+    }
+
+    _update() {
+        const now = Date.now();
+        if (this._emitter) {
+            this._emitter.update((now - this._requestAnimationFrameTimestamp) * DEFAULT_TIME_FACTOR);
+        }
+        this._requestAnimationFrameTimestamp = now;
+        this._pixi_renderer.render(this._pixi_container_root);
+        this._updateAnimationFrameId = requestAnimationFrame(this._update);
     }
 
     empty() {
